d34f13cacf3deb5e4d692892a439751961e4faf1
EthanWakeford <ethanwakeford@gmail.com>
2022-10-26 16:15:01 -0500

    updated

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -10,6 +11,6 @@ class City(BaseModel, Base):
     
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=iFalse)
-        places = relationship('Place', cascade="all, delete, delete-orphan",
-                                          backref='cities')
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    #places = relationship('Place', cascade="all, delete, delete-orphan",
+    #                      backref='cities')
