de87b21adda02ebd77bd6d6729728957adb648c8
EthanWakeford <ethanwakeford@gmail.com>
2022-10-26 15:00:27 -0500

    updated for orm

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, DateTime
 
 Base = declarative_base()
 
@@ -11,8 +11,8 @@ class BaseModel:
     """A base class for all hbnb models"""
     
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(datetime, server_default=utcnow(), nullable=False)
-    updated_at = Column(DateTime, onupdate=utcnow(), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     
     
     def __init__(self, *args, **kwargs):
@@ -50,8 +50,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if _sa_instance_state in dictionary:
-            del dictionary[_sa_instance_state]
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City",
+        backref="State",
+        cascade="all, delete, delete-orphan"
+    )
+
+    @property
+    def cities(self): # +T6
+        """Returns a list of City instances with state_id = id"""
+        cities = []
+        for thing in models.storage.all(City).values():
+            if thing.state_id == self.id:
+                cities.append(thing)
+        return cities
