af06cf9263ca0bb3ae22b947ebc28649f8bccdd4
EthanWakeford <ethanwakeford@gmail.com>
2022-10-27 13:26:22 -0500

    changed do_all method, works with db_storage now

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ Console Module """
 import cmd
 import sys
+from os import getenv
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -211,17 +212,16 @@ class HBNBCommand(cmd.Cmd):
         """ Shows all objects, or all objects of a class"""
         print_list = []
 
-        if args:
-            args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
+        args = args.split(' ')[0]  # remove possible trailing args
+        if getenv("HBNB_TYPE_STORAGE") == "db":
+            storage.all(args)
+        else:
+            if args and args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
             for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
+                if (not args) or (k.split('.')[0] == args):
                     print_list.append(str(v))
-        else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
 
         print(print_list)
 
