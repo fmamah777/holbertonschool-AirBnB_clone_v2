17c38014f5688c0d105e7f392ec2825026794c1e
EthanWakeford <ethanwakeford@gmail.com>
2022-10-28 11:08:17 -0500

    reverting

--- models/base_model.py
+++ models/base_model.py
@@ -49,10 +49,8 @@ class BaseModel:
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
-        if 'created_at' in dictionary:
-            dictionary['created_at'] = self.created_at.isoformat()
-        if 'updated_at' in dictionary:
-            dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary:
             del dictionary['_sa_instance_state']
         return dictionary
