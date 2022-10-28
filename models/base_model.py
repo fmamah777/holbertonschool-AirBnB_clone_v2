1ea292459f8745b7052872c13653ecccbe3c050d
EthanWakeford <ethanwakeford@gmail.com>
2022-10-28 13:28:31 -0500

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,6 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
-            print('used kwargs')
 
     def __str__(self):
         """Returns a string representation of the instance"""
