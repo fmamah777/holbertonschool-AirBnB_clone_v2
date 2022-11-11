5e79606472042b2f36e9bd8d04988a75abdf844a
EthanWakeford <ethanwakeford@gmail.com>
2022-11-11 14:49:31 -0600

    added 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''flask application setup'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    '''prints hello HBHB'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_world_hbnb():
+    '''prints HBHB'''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hbnb_c(text):
+    '''prints text'''
+    return (f'c {text}'.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
