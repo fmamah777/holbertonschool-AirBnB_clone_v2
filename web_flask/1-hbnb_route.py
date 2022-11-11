c613389579714960a631caf0526133e203e724cf
EthanWakeford <ethanwakeford@gmail.com>
2022-11-11 14:38:32 -0600

    removed exclam

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,11 @@ def hello_world():
     '''prints hello HBHB'''
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_world_hbnb():
     '''prints HBHB'''
-    return "HBNB!"
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
