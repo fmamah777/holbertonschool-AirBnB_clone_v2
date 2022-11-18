3d64ea61fbe5cfb7b80e1a05c60ad0d6821d9e9d
fmamah777 <fm.mamah@gmail.com>
2022-11-18 08:45:48 +0000

    made corrections to template files

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,61 +1,51 @@
 #!/usr/bin/python3
-"""script starts Flask web application and defines route '/'"""
-
+""" Start a Flask web application """
 from flask import Flask, render_template
-# create Flask application instance, passing it the name of the current module
 app = Flask(__name__)
 
 
-# decorator turns a python function into a Flask view function
 @app.route('/', strict_slashes=False)
-def hello():
-    """funct responds to requests to the main URL diplaying 'Hello HBNB'"""
+def hello_hbnb():
+    """ Print a hello """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """defining behavior for /hbnb url, display 'HBNB'"""
+    """ Print 'HBNB' """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """defining /c url behavior w dynamic capacity to display text"""
-    return 'C {}'.format(text.replace('_', ' '))
+    """ Print C <text> """
+    return 'C {}'.format(text).replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """define /python/<text> URL behavior"""
-    return 'Python {}'.format(text.replace('_', ' '))
+def _python(text="is cool"):
+    """ Print python <text> """
+    return 'Python {}'.format(text).replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """define behavior of /number/<n> URL where n must be integer"""
+def get_num(n):
+    """ Return n if it is an integer """
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """/number_template/<n> will return an HTML page if n is an int"""
-    # render template will look in templates folder for html file designated
+def get_num_template(n):
+    """ Display an HTML page if n is number """
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def odd_or_even(n):
-    """displays HTML page if N is int and which states if n is odd or even"""
-    if (n % 2) == 0:  # the number is even
-        ntype = 'even'
-    else:  # the number is odd
-        ntype = 'odd'
-    # tell html template what variables from route def apply to html variables
-    return render_template('6-number_odd_or_even.html', n=n, ntype=ntype)
+def get_num_odd_or_even(n):
+    """ Display odd or even if n is a number """
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
-# start the flask application listening on host address and ports specificied
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
